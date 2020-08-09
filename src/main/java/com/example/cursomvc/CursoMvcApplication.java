ad71a149e688e72fca5e9c4f320350eeadce95d5
Isaque Rocha <isaquerocha@gmail.com>
2020-08-09 17:12:45 -0300

    Criando operação de instanciação

--- src/main/java/com/example/cursomvc/CursoMvcApplication.java
+++ src/main/java/com/example/cursomvc/CursoMvcApplication.java
@@ -1,13 +1,29 @@
 package com.example.cursomvc;
 
+import com.example.cursomvc.domain.Categoria;
+import com.example.cursomvc.repositories.CategoriaRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import java.util.Arrays;
+
 @SpringBootApplication
-public class CursoMvcApplication {
+public class CursoMvcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursoMvcApplication.class, args);
 	}
 
+	@Override
+	public void run(String... args) throws Exception {
+		Categoria cat1 = new Categoria(null, "Informática");
+		Categoria cat2 = new Categoria(null, "Escritório");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
--- src/main/java/com/example/cursomvc/resources/CategoriaResource.java
+++ src/main/java/com/example/cursomvc/resources/CategoriaResource.java
@@ -1,6 +1,5 @@
 package com.example.cursomvc.resources;
 
-
 import com.example.cursomvc.domain.Categoria;
 import com.example.cursomvc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -9,9 +8,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-
-import java.util.ArrayList;
-import java.util.List;
 
 @RestController
 @RequestMapping(value = "/categorias")
