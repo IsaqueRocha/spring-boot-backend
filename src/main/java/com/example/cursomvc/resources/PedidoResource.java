24e8536f8a4a1055ae942ca3746066aeb8339160
Isaque Rocha <isaquerocha@gmail.com>
2020-08-10 03:47:39 -0300

    Endpoint /pedidos/{id} disponivel

---
+++ src/main/java/com/example/cursomvc/resources/PedidoResource.java
@@ -0,0 +1,25 @@
+package com.example.cursomvc.resources;
+
+import com.example.cursomvc.domain.Pedido;
+import com.example.cursomvc.services.PedidoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/pedidos")
+public class PedidoResource {
+
+    @Autowired
+    private PedidoService service;
+
+    @GetMapping(value = "/{id}")
+    public ResponseEntity<Pedido> find(@PathVariable Integer id) {
+        Pedido obj = service.buscar(id);
+
+        return ResponseEntity.ok().body(obj);
+    }
+}
---
+++ src/main/java/com/example/cursomvc/services/PedidoService.java
@@ -0,0 +1,22 @@
+package com.example.cursomvc.services;
+
+import com.example.cursomvc.domain.Pedido;
+import com.example.cursomvc.repositories.PedidoRepository;
+import com.example.cursomvc.services.exceptions.ObjectNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Optional;
+
+@Service
+public class PedidoService {
+    @Autowired
+    private PedidoRepository repo;
+
+    public Pedido buscar(Integer id) {
+        Optional<Pedido> obj = repo.findById(id);
+        return obj.orElseThrow(() -> new ObjectNotFoundException(
+                "Objeto não encontrado! ID: " + id + " , Tipo: " + Pedido.class.getName())
+        );
+    }
+}
