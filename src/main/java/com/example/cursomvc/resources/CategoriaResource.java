638ea9ee39b497587a7813f2d02eded8e7771bd7
Isaque Rocha <isaquerocha@gmail.com>
2020-08-11 14:27:24 -0300

    Listando todas as categorias com DTO

--- src/main/java/com/example/cursomvc/resources/CategoriaResource.java
+++ src/main/java/com/example/cursomvc/resources/CategoriaResource.java
@@ -1,6 +1,7 @@
 package com.example.cursomvc.resources;
 
 import com.example.cursomvc.domain.Categoria;
+import com.example.cursomvc.dto.CategoriaDTO;
 import com.example.cursomvc.services.CategoriaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -8,6 +9,8 @@ import org.springframework.web.bind.annotation.*;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import java.net.URI;
+import java.util.List;
+import java.util.stream.Collectors;
 
 @RestController
 @RequestMapping(value = "/categorias")
@@ -15,6 +18,13 @@ public class CategoriaResource {
 
     @Autowired
     private CategoriaService service;
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> findAll() {
+        List<Categoria> list = service.findAll();
+        List<CategoriaDTO> listDto = list.stream().map(obj -> new CategoriaDTO(obj)).collect(Collectors.toList());
+        return ResponseEntity.ok().body(listDto);
+    }
 
     @GetMapping(value = "/{id}")
     public ResponseEntity<Categoria> find(@PathVariable Integer id) {
--- src/main/java/com/example/cursomvc/services/CategoriaService.java
+++ src/main/java/com/example/cursomvc/services/CategoriaService.java
@@ -8,12 +8,17 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
 import java.util.Optional;
 
 @Service
 public class CategoriaService {
     @Autowired
     private CategoriaRepository repo;
+
+    public List<Categoria> findAll() {
+        return repo.findAll();
+    }
 
     public Categoria find(Integer id) {
         Optional<Categoria> obj = repo.findById(id);
