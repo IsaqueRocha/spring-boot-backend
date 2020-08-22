49bb54ca3526813ab0bf35d18562afb491395523
Isaque Rocha <isaquerocha@gmail.com>
2020-08-22 00:02:31 -0300

    Criando profile de teste

---
+++ src/main/java/com/example/cursomvc/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.example.cursomvc.config;
+
+import com.example.cursomvc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("test")
+class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        dbService.instantiateTestDataBase();
+        return true;
+    }
+}
