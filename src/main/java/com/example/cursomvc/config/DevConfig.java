4fb2a6207f80eb96e166095d4274ec8e05725dad
Isaque Rocha <isaquerocha@gmail.com>
2020-08-22 00:27:54 -0300

    Criando profile de desenvolvimento

---
+++ src/main/java/com/example/cursomvc/config/DevConfig.java
@@ -0,0 +1,31 @@
+package com.example.cursomvc.config;
+
+import com.example.cursomvc.services.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.text.ParseException;
+
+@Configuration
+@Profile("dev")
+class DevConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Value("${spring.jpa.hibernate.ddl-auto}")
+    private String strategy;
+
+    @Bean
+    public boolean instantiateDatabase() throws ParseException {
+        if (!"create".equals(strategy)) {
+            return false;
+        }
+
+        dbService.instantiateTestDataBase();
+        return true;
+    }
+}
